50cbed5524622eb5e390bdcacaef4dfa34d74bae
GDJ56 <GDJ56@DESKTOP-7Q6JA4Q>
2022-11-25 08:48:50 +0900

    01_필터 전체커밋

---
+++ 02_filter_listener/src/main/java/com/filter/common/listener/ContextListenerTest.java
@@ -0,0 +1,22 @@
+package com.filter.common.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextListenerTest 
+	implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		//서버가 종료됐을 때
+		System.out.println("서버가 종료됨");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		//서버가 실행됐을 때
+		System.out.println("서버가 실행됨");
+	}
+	
+	
+}
